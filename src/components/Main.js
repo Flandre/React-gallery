d7cf3363ef8557fe1483a54235afecf2ddb74eb5
Flandre <flantan233@gmail.com>
2016-06-22 16:41:29 +0800

    import images

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,22 @@ import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
+let imageDatas = require('../data/imageDatas.json');
+
+function genImageURL(imageDatasArr) {
+  for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+    var singleImageData = imageDatasArr[i];
+    singleImageData.imageURL = require('../data/' + singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+}
+
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
+        <img src={yeomanImage} alt="Yeoman Generator"/>
         <span>hello</span>
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
@@ -17,7 +28,6 @@ class AppComponent extends React.Component {
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
